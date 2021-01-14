3557b1bf18028bdc1bff97ae9e0e02802330452f
Lorenzo-Arnau <skyline03@hotmail.it>
2021-01-14 17:31:25 +0100

    Function Hover disabled, adding alert on click internal menu selectors

--- js/script.js
+++ js/script.js
@@ -3,11 +3,15 @@ $(document).ready(function(){
 $('.menu-two li').click(function(){
   $( this ).find( ".content" ).toggle();
 });
-$('.menu-two li').hover(function(){
-$( this ).find( ".content" ).toggle();
-},function(){
-$( this ).find( ".content" ).toggle();
+$('.content li').click(function(){
+  alert('Lavori in corso! Questa sezione non è momentaneamente disponibile')
 });
+
+// $('.menu-two li').hover(function(){
+// $( this ).find( ".content" ).toggle();
+// },function(){
+// $( this ).find( ".content" ).toggle();
+// });
 
 })
 // End version checked =^-^=
